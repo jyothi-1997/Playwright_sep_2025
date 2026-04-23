f93d25f98181615aa8d5846b878c43d8e33ca43a
jyothi-1997 <jyothikummitha1997@gmail.com>
2026-04-23 16:03:18 +0530

    Added the login

--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -33,6 +33,19 @@ How do we work with "Version Control Tool"?
             6. "git status" - to check the files which are modified
             6. "git add ." - to stage all the modified files
             7. "git commit -m "Added the login page test cases"" - to commit the changes
+            8. "git commit -m "Added the login page test cases"" - to commit the changes
+     8.1 - Switch to the main branch and make pull - git checkout master - git pull
+    8.2 - Switch back to your feature branch - "git checkout TS01LoginPage"
+    8.3 - Merge the main branch changes to your feature branch - "git merge master"
+9. "git push origin TS01LoginPage" - to push the changes to the remote branch
+10. Go to the github/bitbucket/gitlab portal - you will see a pop up to create a pull request
+11. Create a pull request for the changes - assign the reviewer and create the PR
+12. Once the reviewer approves the changes - merge the changes to the main branch
+
+3.0 - Pull all the changes inside you local system from the main/master branch by moving to the main branch inside your local system
+"git checkout main" - to move to the main branch
+"git pull" - to pull all the changes from the remote main branch to your local main branch
+Repeat #3-#12 everytime
 
 
 
